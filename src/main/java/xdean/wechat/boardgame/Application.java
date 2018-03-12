5a03ae588c50d62357a239c6b6cae4324bca21e9
XDean <373216024@qq.com>
2018-03-12 16:09:19 +0800

    mark

---
+++ src/main/java/xdean/wechat/boardgame/Application.java
@@ -0,0 +1,10 @@
+package xdean.wechat.boardgame;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+}
