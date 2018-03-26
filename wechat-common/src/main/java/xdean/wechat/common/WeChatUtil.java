6471c180e230554c0b560824375e248003b85a03
XDean <373216024@qq.com>
2018-03-26 11:00:05 +0800

    fix bug and add log

--- wechat-common/src/main/java/xdean/wechat/common/WeChatUtil.java
+++ wechat-common/src/main/java/xdean/wechat/common/WeChatUtil.java
@@ -5,11 +5,17 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+import xdean.jex.log.Log;
+import xdean.jex.log.LogFactory;
 import xdean.jex.util.security.SecurityUtil;
 
 public class WeChatUtil {
 
+  private static final Log LOG = LogFactory.from(WeChatUtil.class);
+
   public static boolean checkSignature(String token, String signature, String timestamp, String nonce) {
+    LOG.debug(
+        String.format("Check Signature: token=%s, signature=%s, timestamp=%s, nonce=%s", token, signature, timestamp, nonce));
     String[] arr = new String[] { token, timestamp, nonce };
     Arrays.sort(arr);
     StringBuilder content = new StringBuilder();
@@ -17,9 +23,8 @@ public class WeChatUtil {
       content.append(arr[i]);
     }
     String digest = null;
-
     try {
-      SecurityUtil.digest(new ByteArrayInputStream(content.toString().getBytes()), "SHA-1");
+      digest = SecurityUtil.digest(new ByteArrayInputStream(content.toString().getBytes()), "SHA-1");
     } catch (NoSuchAlgorithmException | IOException e) {
       e.printStackTrace();
     }
