32e44e64e813bcfbbcc99b10b56117de3f1e4e35
XDean <373216024@qq.com>
2018-03-26 14:52:13 +0800

    update checkSignature

--- wechat-common/src/main/java/xdean/wechat/common/WeChatUtil.java
+++ wechat-common/src/main/java/xdean/wechat/common/WeChatUtil.java
@@ -1,47 +1,36 @@
 package xdean.wechat.common;
 
+import static xdean.jex.util.function.Predicates.isEquals;
+
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Objects;
 
 import org.springframework.web.client.RestTemplate;
 
-import com.google.common.collect.ImmutableMap;
-
+import io.reactivex.Observable;
 import xdean.jex.log.Log;
 import xdean.jex.log.LogFactory;
 import xdean.jex.util.security.SecurityUtil;
+import xdean.wechat.common.model.AccessToken;
 
 public class WeChatUtil implements WeChatConstants {
 
   private static final Log LOG = LogFactory.from(WeChatUtil.class);
 
   public static boolean checkSignature(String token, String signature, String timestamp, String nonce) {
-    LOG.debug(
-        String.format("Check Signature: token=%s, signature=%s, timestamp=%s, nonce=%s", token, signature, timestamp, nonce));
-    String[] arr = new String[] { token, timestamp, nonce };
-    Arrays.sort(arr);
-    StringBuilder content = new StringBuilder();
-    for (int i = 0; i < arr.length; i++) {
-      content.append(arr[i]);
-    }
-    String digest = null;
-    try {
-      digest = SecurityUtil.digest(new ByteArrayInputStream(content.toString().getBytes()), "SHA-1");
-      LOG.debug("Calculate result: " + digest);
-    } catch (NoSuchAlgorithmException | IOException e) {
-      e.printStackTrace();
-    }
-    content = null;
-    return Objects.equals(digest, signature);
+    LOG.debug(String.format("Check Signature: token=%s, signature=%s, timestamp=%s, nonce=%s",
+        token, signature, timestamp, nonce));
+    return Observable.fromArray(token, timestamp, nonce)
+        .sorted()
+        .reduce((a, b) -> a + b)
+        .map(s -> SecurityUtil.digest(new ByteArrayInputStream(s.getBytes()), "SHA-1"))
+        .map(isEquals(signature)::test)
+        .blockingGet(false);
   }
 
-  public static String getToken(String appid, String secret) {
-    final String url = WECHAT_URL + "/cgi-bin/token?grant_type=client_credential&{appid}&{secret}";
+  public static AccessToken getToken(WeChatVars wcv) {
+    String url = WECHAT_URL + "/cgi-bin/token?grant_type=client_credential&appid="
+        + wcv.appId + "&secret=" + wcv.appSecret;
     RestTemplate rt = new RestTemplate();
-    String result = rt.getForObject(url, String.class, ImmutableMap.of("appid", appid, "secret", secret));
-    return result;
+    return rt.getForObject(url, AccessToken.class);
   }
 }
--- wechat-common/src/test/java/xdean/wechat/common/WeChatUtilTest.java
+++ wechat-common/src/test/java/xdean/wechat/common/WeChatUtilTest.java
@@ -10,9 +10,4 @@ public class WeChatUtilTest {
     assertTrue(WeChatUtil.checkSignature("test", "bb5f8aca3af358288f3fb866b7580e9378be123d", "1522033654", "651288971"));
     assertFalse(WeChatUtil.checkSignature("no", "bb5f8aca3af358288f3fb866b7580e9378be123d", "1522033654", "651288971"));
   }
-
-  @Test
-  public void testToken() throws Exception {
-    System.out.println(WeChatUtil.getToken("a", "b"));
-  }
 }
