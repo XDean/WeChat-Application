62e6dd039d69239bc7358de19436e60a786210b6
XDean <373216024@qq.com>
2018-03-26 19:58:41 +0800

    add @PostXml

--- wechat-app/src/main/java/xdean/wechat/app/controller/MessageController.java
+++ wechat-app/src/main/java/xdean/wechat/app/controller/MessageController.java
@@ -1,15 +1,15 @@
 package xdean.wechat.app.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import xdean.jex.log.Logable;
+import xdean.wechat.common.annotation.PostXml;
 import xdean.wechat.common.model.message.TextMessage;
 
 @RestController
 public class MessageController implements Logable {
-  @PostMapping(path = "wechat", consumes = "text/xml")
+  @PostXml(path = "wechat")
   public TextMessage postMessage(@RequestBody TextMessage text) {
     info(text.toString());
     return text;
