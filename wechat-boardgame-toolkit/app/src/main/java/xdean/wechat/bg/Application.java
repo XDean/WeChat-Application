3c5f30163cc10e5cbbaa263dcd193cecc0e4ece3
XDean <373216024@qq.com>
2018-04-12 15:14:42 +0800

    extract webconfig

--- wechat-boardgame-toolkit/app/src/main/java/xdean/wechat/bg/Application.java
+++ wechat-boardgame-toolkit/app/src/main/java/xdean/wechat/bg/Application.java
@@ -2,54 +2,12 @@ package xdean.wechat.bg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @EnableWebSecurity
 @SpringBootApplication
-public class Application extends WebSecurityConfigurerAdapter {
-
+public class Application {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        .csrf().disable()
-        .authorizeRequests()
-        .antMatchers("/root/**").authenticated()
-        .anyRequest().permitAll()
-        .and().formLogin().permitAll()
-        .and().logout().permitAll();
-  }
-
-  @Bean
-  @Override
-  @SuppressWarnings("deprecation")
-  public UserDetailsService userDetailsService() {
-    return new InMemoryUserDetailsManager(User.withDefaultPasswordEncoder()
-        .username("dean")
-        .password("dean")
-        .roles("user")
-        .build());
-  }
-
-  @Bean
-  public ServletRegistrationBean<DispatcherServlet> dispatcherRegistration() {
-    return new ServletRegistrationBean<>(dispatcherServlet());
-  }
-
-  @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
-  public DispatcherServlet dispatcherServlet() {
-    return new LoggableDispatcherServlet();
   }
 }
---
+++ wechat-boardgame-toolkit/app/src/main/java/xdean/wechat/bg/config/WebConfig.java
@@ -0,0 +1,58 @@
+package xdean.wechat.bg.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+import xdean.wechat.bg.LoggableDispatcherServlet;
+
+@Configuration
+@EnableEncryptableProperties
+@PropertySource("classpath:web.properties")
+public class WebConfig extends WebSecurityConfigurerAdapter {
+  private @Value("${web.admin.name}") String name;
+  private @Value("${web.admin.password}") String password;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/root/**").authenticated()
+        .anyRequest().permitAll()
+        .and().formLogin().permitAll()
+        .and().logout().permitAll();
+  }
+
+  @Bean
+  @Override
+  @SuppressWarnings("deprecation")
+  public UserDetailsService userDetailsService() {
+    return new InMemoryUserDetailsManager(User.withDefaultPasswordEncoder()
+        .username(name)
+        .password(password)
+        .roles("admin")
+        .build());
+  }
+
+  @Bean
+  public ServletRegistrationBean<DispatcherServlet> dispatcherRegistration() {
+    return new ServletRegistrationBean<>(dispatcherServlet());
+  }
+
+  @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+  public DispatcherServlet dispatcherServlet() {
+    return new LoggableDispatcherServlet();
+  }
+}
