9c593c94a03dafd2d208fafccd9fb5c7fd42f5a4
XDean <373216024@qq.com>
2018-05-17 22:34:34 +0800

    temp remove checker annotation

--- wechat-boardgame-toolkit/common/src/main/java/xdean/wechat/bg/message/CommonMessageHolder.java
+++ wechat-boardgame-toolkit/common/src/main/java/xdean/wechat/bg/message/CommonMessageHolder.java
@@ -2,6 +2,6 @@ package xdean.wechat.bg.message;
 
 import xdean.auto.message.AutoMessage;
 
-@AutoMessage(path = "messages-common.properties", generatedName = "CommonMessages")
+@AutoMessage(path = "/message/messages-common.properties", generatedName = "CommonMessages")
 public class CommonMessageHolder {
 }
--- wechat-boardgame-toolkit/common/src/main/java/xdean/wechat/bg/service/GameStateHandler.java
+++ wechat-boardgame-toolkit/common/src/main/java/xdean/wechat/bg/service/GameStateHandler.java
@@ -3,13 +3,12 @@ package xdean.wechat.bg.service;
 import java.util.Arrays;
 import java.util.List;
 
-import xdean.deannotation.checker.CheckChildren;
 import xdean.wechat.bg.model.GameCommand;
 import xdean.wechat.bg.model.Player;
 import xdean.wechat.common.spring.OrderListCollector;
 import xdean.wechat.common.spring.TextWrapper;
 
-@CheckChildren(annotated = xdean.wechat.bg.annotation.StateHandler.class)
+//@CheckChildren(annotated = xdean.wechat.bg.annotation.StateHandler.class)
 public interface GameStateHandler {
   TextWrapper handle(Player player, GameCommand<?> command);
 
