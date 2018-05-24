fe4c0c92f2679ba9eacd90d6516bc419563d1a3a
XDean <373216024@qq.com>
2018-05-24 20:57:07 +0800

    remove jpa

--- wechat-boardgame-toolkit/app/src/main/java/xdean/wechat/bg/config/DatabaseConfig.java
+++ wechat-boardgame-toolkit/app/src/main/java/xdean/wechat/bg/config/DatabaseConfig.java
@@ -5,11 +5,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 
-@Configuration
+//@Configuration
 @EncryptablePropertySource("classpath:database.properties")
 public class DatabaseConfig {
 
